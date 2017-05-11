03e382e434d640798ef3d8fe7738b174732e3563
hmassonn <hmassonn@student42.fr>
2017-05-11 18:18:06 +0200

    new layout

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,7 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('header', { title: 'Express' });
- 	res.render('index', { title: 'Express' });
+ 	res.render('index', { title: 'Express', pageController: "IndexController" });
 });
 
 module.exports = router;
