a5619f1cdc34c38baa04dc0316e8130209c91972
Marine JEANNIN <mjeannin@e2r11p17.42.fr>
2017-05-12 13:59:52 +0200

    begin routing

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -45,6 +45,11 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+app.post('/post.html', function(request, response){
+	var p1 = request.body.p1; 
+  	console.log("p1=" + p1);
 });
 
 module.exports = app;
