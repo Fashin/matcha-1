8327c245594a34cec3e5330fd37d7ce27514e871
hmassonn <hmassonn@student42.fr>
2017-11-27 16:22:04 +0100

    merge

--- routes/picture.js
+++ routes/picture.js
@@ -3,22 +3,30 @@ const session	= require("express-session");
 const flash		= require('express-flash');
 const database	= require('../model/database');
 const router 	= express.Router();
+var fs = require('fs');
 
 router.post('/', function(req, res) {
 	console.log("POST /picture");
-	    const User = {
-	        photos: req.body.picture,
-	    };
-	database.query('UPDATE users SET ? WHERE login = ?', [User, req.session.login], function (error, results, fields) {
+    const User = {
+		photos: req.session.login
+    };
+	fs.writeFile("/tmp/"+User.photos, req.body.picture, function(err) {
+	    if(err) {
+	        return console.log(err);
+	    }
+	    console.log("The file was saved!");
+	});
+	database.query('UPDATE users SET photos = ? WHERE login = ?', [req.session.login, req.session.login], function (error, results, fields) {
         if (error) throw error;
         console.log(results);
-        if (results.affectedRows == 1) {
+        if (results != undefined && results.affectedRows == 1) {
 			req.flash('notice', "l'image a bien ete upload" );
-            req.session.login = User.login;
-            res.status(201).send("Profil mis à jour");
-        }
-        else
-            res.status(500).send("Nous n'avons pas pu modifier vos informations, ce n'est pas vous c'est nous, nous sommes désolés :(");
+            res.redirect("/myprofile");
+        } else {
+			req.flash('error', "Nous n'avons pas pu modifier vos informations, ce n'est pas vous c'est nous, nous sommes désolés :(" );
+			res.redirect("/myprofile");
+		}
+
     });
 });
 
